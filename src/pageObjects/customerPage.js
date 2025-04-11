8d896ae544d7e924f107625dca1ca4b42aa2f3f9
ahmed bakheet <ahmed.s.bakheet@gmail.com>
2025-04-11 15:56:21 +0200

    refactor the project to use modern js instead of classes

--- src/pageObjects/customerPage.js
+++ src/pageObjects/customerPage.js
@@ -1,39 +1,37 @@
-import { request, expect } from "@playwright/test";
+import { request, expect } from '@playwright/test';
 
 const APP_HOST = process.env.BASE_URL;
 const PET_CUSTOMER_ID = process.env.PET_CUSTOMER_ID;
 const AGENT_EMAIL = process.env.AGENT_EMAIL;
 const AGENT_TOKEN = process.env.AGENT_TOKEN;
 const X_API_KEY = process.env.X_API_KEY;
-class CustomerPage {
-  constructor(page) {
-    this.page = page;
-  }
 
-  async navigateTo() {
-    console.log(PET_CUSTOMER_ID);
-    await this.page.goto(`${APP_HOST}/admin/customers/${PET_CUSTOMER_ID}`);
-  }
-  async createClaim() {
-    await this.navigateTo();
-    await this.page.getByRole("button", { name: "Mehr " }).click();
-    await this.page.getByText("Neuer Leistungsfall").click();
-    await this.page.getByRole("button", { name: "Claim anlegen" }).click();
-    await this.page.waitForTimeout(5000);
-  }
-  async deleteClaim() {
-    const url = this.page.url();
-    const claimId = url.substring(url.lastIndexOf("/") + 1);
-    console.log(`Claim ID: ${claimId}`);
-    const apiContext = await request.newContext({
-      baseURL: APP_HOST,
-      extraHTTPHeaders: {
-        "x-api-key": X_API_KEY,
-      },
-    });
-    const deleteUrl = `/api/v1/claims/${claimId}?agent_email=${AGENT_EMAIL}&agent_token=${AGENT_TOKEN}`;
-    const response = await apiContext.delete(deleteUrl);
-    expect(response.status()).toBe(200);
-  }
+export async function navigateTo(page) {
+  console.log(PET_CUSTOMER_ID);
+  await page.goto(`${APP_HOST}/admin/customers/${PET_CUSTOMER_ID}`);
 }
-export default CustomerPage;
+
+export async function createClaim(page) {
+  await navigateTo(page);
+  await page.getByRole('button', { name: 'Mehr ' }).click();
+  await page.getByText('Neuer Leistungsfall').click();
+  await page.getByRole('button', { name: 'Claim anlegen' }).click();
+  await page.waitForTimeout(5000);
+}
+
+export async function deleteClaim(page) {
+  const url = page.url();
+  const claimId = url.substring(url.lastIndexOf('/') + 1);
+  console.log(`Claim ID: ${claimId}`);
+
+  const apiContext = await request.newContext({
+    baseURL: APP_HOST,
+    extraHTTPHeaders: {
+      'x-api-key': X_API_KEY,
+    },
+  });
+
+  const deleteUrl = `/api/v1/claims/${claimId}?agent_email=${AGENT_EMAIL}&agent_token=${AGENT_TOKEN}`;
+  const response = await apiContext.delete(deleteUrl);
+  expect(response.status()).toBe(200);
+}
--- src/tests/adminPanel/claims/petClaims.spec.js
+++ src/tests/adminPanel/claims/petClaims.spec.js
@@ -1,7 +1,8 @@
-import { test, expect } from "../../setupPage";
-require("dotenv").config();
+import { test } from '@playwright/test';
+import { createClaim, deleteClaim } from '../../../pageObjects/customerPage';
+require('dotenv').config();
 
-test("Create a new claim and delete it", async ({ customerPage }) => {
-  await customerPage.createClaim();
-  await customerPage.deleteClaim()
+test('Create a new claim and delete it', async ({ page }) => {
+  await createClaim(page);
+  await deleteClaim(page);
 });
